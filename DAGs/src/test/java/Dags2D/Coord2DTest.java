9a9da3ab8ffa8f4ab14a6dd1a0a1782d46c8721e
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-11-21 13:15:45 +0300

    Tests for Coord2D and Space singleton done

---
+++ DAGs/src/test/java/Dags2D/Coord2DTest.java
@@ -0,0 +1,22 @@
+package Dags2D;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Coord2DTest {
+    private Coord2D instance;
+
+    @Test
+    void testEquals() {
+        assertEquals(new Coord2D(3.12, 5), new Coord2D(3.12, 5));
+        assertNotEquals(new Coord2D(3, 5), new Coord2D(3.12, 5));
+        assertNotEquals(3.12, new Coord2D(3.12, 5));
+        assertNotEquals(null, new Coord2D(3.12, 5));
+        Coord2D coord = new Coord2D(6, 1.123);
+        assertEquals(coord, coord);
+
+    }
+
+}
