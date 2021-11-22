b9ff95043abc66b4c4eea41da3d29b4c5801b422
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-11-22 13:38:42 +0300

    Input interface fixes

--- DAGs/src/main/java/interfaces/Input.java
+++ DAGs/src/main/java/interfaces/Input.java
@@ -12,12 +12,6 @@ public interface Input {
     boolean enterBoolean(String prompt, String errorMessage, int attempts);
 
     /**
-     * Cell parser.
-     */
-    int[] enterCell(int from1, int to1, int from2, int to2,
-                    String prompt, String errorMessage, int attempts, String action1, String action2);
-
-    /**
      * Int parser.
      */
     int parseInt(int from, int to, String prompt, String errorMessage, int attempts);
