13dc66dd9b22a89ea13a2a25aa3518cf6a3e9289
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-11-22 13:32:28 +0300

    Interfaces added

---
+++ DAGs/src/main/java/interfaces/ApiUser.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface ApiUser {
+    void useDagApi();
+}
---
+++ DAGs/src/main/java/interfaces/Input.java
@@ -0,0 +1,29 @@
+package interfaces;
+
+public interface Input {
+    /**
+     * From string int parser.
+     */
+    int parseIntFromString(String input, int from, int to);
+
+    /**
+     * Boolean parser.
+     */
+    boolean enterBoolean(String prompt, String errorMessage, int attempts);
+
+    /**
+     * Cell parser.
+     */
+    int[] enterCell(int from1, int to1, int from2, int to2,
+                    String prompt, String errorMessage, int attempts, String action1, String action2);
+
+    /**
+     * Int parser.
+     */
+    int parseInt(int from, int to, String prompt, String errorMessage, int attempts);
+
+    /**
+     * Get line.
+     */
+    String getLine();
+}
