b0d199094752d617db04adb4cb276b3d9ffe2dc9
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-11-30 19:53:53 +0300

    Space done

--- DAGs/src/main/java/Dags2D/Space.java
+++ DAGs/src/main/java/Dags2D/Space.java
@@ -2,11 +2,18 @@ package Dags2D;
 
 import Dags2D.exceptions.EmptyBoundsException;
 
-public final class Space {
+import java.io.Serializable;
+import java.util.Objects;
+
+public final class Space implements Serializable {
     private final Origin root;
     private BoundBox bounds;
 
     public Space(Origin origin) {
+        if (origin == null) {
+            throw new IllegalArgumentException("Origin can not be null");
+        }
+
         root = origin;
     }
 
@@ -16,9 +23,8 @@ public final class Space {
 
     public BoundBox getWorldBounds() throws EmptyBoundsException {
         BoundBox rootBounds = root.getBounds();
-        bounds = new BoundBox(rootBounds.getLeftLowerPoint().offset(root.getPosition().getX(), root.getPosition().getY()),
-                rootBounds.getRightUpperPoint().offset(root.getPosition().getX(), root.getPosition().getY())
-        );
+        bounds = new BoundBox(rootBounds.getLeftLowerCoords().offset(root.getPosition().getX(), root.getPosition().getY()),
+                rootBounds.getRightUpperCoords().offset(root.getPosition().getX(), root.getPosition().getY()));
 
         return bounds;
     }
@@ -28,5 +34,23 @@ public final class Space {
         return "Space{" +
                 "root=" + root +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+
+        Space space = (Space) other;
+        return root.equals(space.root);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(root.getPosition());
     }
 }
