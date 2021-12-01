dfe4199d8a208a5d66424118d04d10870df88e4d
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-12-01 13:09:51 +0300

    Message added

--- DAGs/src/main/java/Dags2D/exceptions/DAGConstraintException.java
+++ DAGs/src/main/java/Dags2D/exceptions/DAGConstraintException.java
@@ -1,5 +1,28 @@
 package Dags2D.exceptions;
 
-public class DAGConstraintException extends Exception {
+import Dags2D.Point;
 
+/**
+ * Exception to be thrown if origin does not contain any physical points.
+ */
+public class DAGConstraintException extends Exception {
+    private final Point cycling;
+
+    public DAGConstraintException() {
+        this(null);
+    }
+
+    public DAGConstraintException(Point emptyOrigin) {
+        super();
+        this.cycling = emptyOrigin;
+    }
+
+    @Override
+    public String getMessage() {
+        if (cycling == null) {
+            return "DAG has cycles";
+        }
+
+        return "Origin " + cycling + " has cycles";
+    }
 }
