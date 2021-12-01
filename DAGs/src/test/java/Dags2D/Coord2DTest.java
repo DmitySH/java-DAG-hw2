bfed5221be572fa344f96d178372f846b590c905
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-12-01 11:49:54 +0300

    Coord2DTest done

--- DAGs/src/test/java/Dags2D/Coord2DTest.java
+++ DAGs/src/test/java/Dags2D/Coord2DTest.java
@@ -1,11 +1,19 @@
 package Dags2D;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Coord2DTest {
     private Coord2D instance;
+
+    @BeforeEach
+    void init() {
+        instance = new Coord2D(1.3, -2);
+    }
 
     @Test
     void testEquals() {
@@ -31,5 +39,42 @@ class Coord2DTest {
         Coord2D second = new Coord2D(2, 2);
 
         assertEquals(Coord2D.coordRightUpper(first, second), new Coord2D(2, 3));
+    }
+
+    @Test
+    void testGetX() {
+        assertEquals(instance.getX(), 1.3);
+    }
+
+    @Test
+    void testGetY() {
+        assertEquals(instance.getY(), -2);
+    }
+
+    @Test
+    void testHashCode() {
+        assertEquals(instance.hashCode(), Objects.hash(instance.getX(), instance.getY()));
+    }
+
+    @Test
+    void testMakeCopy() {
+        Coord2D copy = Coord2D.makeCopy(instance);
+
+        assertNotSame(copy, instance);
+        assertEquals(copy, instance);
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("{x=1.3, y=-2.0}", instance.toString());
+    }
+
+    @Test
+    void testOffset() {
+        Coord2D ofsetted = instance.offset(1, -1);
+        Coord2D sameCoords = new Coord2D(2.3, -3);
+
+        assertEquals(ofsetted, sameCoords);
+        assertNotSame(ofsetted, instance);
     }
 }
